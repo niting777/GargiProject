7a1b7127c66c35faf0b7208fa8cc61a6c10a82fa
HP <HP@DESKTOP-8D6SG2F>
2024-05-30 20:23:18 +0530

    adding Test project

---
+++ GargiArtifactId/src/main/java/javaPackage/Test1.java
@@ -0,0 +1,20 @@
+package javaPackage;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void b() {
+		System.out.println("Hello TestNg");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello main Method");
+	}
+
+}
